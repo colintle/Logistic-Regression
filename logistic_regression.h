931d97c4d5a9dfbd8c0bc3cf4973774694f88ece
Colin Le <colintranle123@gmail.com>
2025-11-09 16:27:41 -0800

    submodule broken

--- logistic_regression.h
+++ logistic_regression.h
@@ -6,7 +6,7 @@
 // N is the number of samples
 // M is the number of features
 // K is the number of samples to predict
-// Output dimension is 1 for binary classification (0 or 1)
+// C is the number of classes
 class LogisticRegression {
 public:
   LogisticRegression(size_t input_size, double learning_rate, int iterations);
@@ -15,25 +15,29 @@ public:
   LogisticRegression &operator=(const LogisticRegression &) = delete;
 
   // X shape: (N, M)
-  // y shape: (N, 1)
+  // y shape: (N, 1) with class labels (0 to C-1)
   void fit(const matrix_library::Tensor<double> &X,
            const matrix_library::Tensor<double> &y);
   // X shape: (K, M)
-  // Output shape: (K, 1)
-  double predict(const matrix_library::Tensor<double> &X) const;
+  // Output shape: (K, C) with probabilities for each class
+  matrix_library::Tensor<double>
+  predict(const matrix_library::Tensor<double> &X) const;
 
   const matrix_library::Tensor<double> &get_weights() const;
   const matrix_library::Tensor<double> &get_bias() const;
 
 private:
-  double sigmoid(double z) const;
-
+  // Row shape: (1, C)
+  // Modifies the input row in-place
+  static void softmax(std::vector<double> &row);
   double learning_rate_;
   int iterations_;
-  // Weights shape: (M, 1)
+  // Weights shape: (M, C)
   matrix_library::Tensor<double> weights_;
-  // Bias shape: (1, 1)
+  // Bias shape: (1, C)
   matrix_library::Tensor<double> bias_;
+  // Number of classes
+  size_t num_classes_ = 0;
 };
 
 #endif // LOGISTIC_REGRESSION_H
